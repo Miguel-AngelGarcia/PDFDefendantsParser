9db336c28d6ca60e6050fc98f771c1d79abb0e59
Miguel-AngelGarcia <mgar574@wgu.edu>
2022-10-25 18:02:27 -0400

    We are in the process of updating our work cycle.

--- pdftotexttest.py
+++
@@ -1,8 +0,0 @@
-import pdftotext
-
-# Load your PDF
-with open("Practices/0001545 - United States of America v. Califo.pdf", "rb") as f:
-    pdf = pdftotext.PDF(f)
-
-for page in pdf:
-    print(page)
--- strng.py
+++
@@ -1,2 +0,0 @@
-string = 'hi i am miguel what is up'
-print(string[7:11])
